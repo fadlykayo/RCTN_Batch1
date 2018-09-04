1486bcd7b5c162a0ecacb01fdaa70afd4ea76288
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-09-04 11:22:33 +0700

    Add resources for next session

--- Day3.js
+++ Day3.js
@@ -353,6 +353,6 @@ const askMom = async() => {
 
 askMom();
 
-// Resources for next week:
+// Resources for next session:
 // https://medium.com/skyshidigital/1-pendahuluan-seminggu-menjadi-master-react-js-seri-tingkat-dasar-7b6ec7b04af
 // https://medium.freecodecamp.org/learn-react-js-in-5-minutes-526472d292f4
