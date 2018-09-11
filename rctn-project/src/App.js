331e0a16a21a2c750a001b4dc68f6a13b1281c38
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-09-12 00:52:09 +0700

    Edit Qtemu App

--- rctn-project/src/App.js
+++ rctn-project/src/App.js
@@ -5,16 +5,75 @@ export default class App extends Component {
   renderHeader() {
     return (
       <div className="header">
-        <h1 style={{color: 'white'}}>JUDUL</h1>
+        <div className="header_left">
+          <a className="header_title" href="#">Qtemu</a>
+          <a className="header_title" href="#">Create Meetup</a>
+          <a className="header_title" href="#">Explore</a>
+        </div>
+        <a className="header_title">Login</a>
+      </div>
+    );
+  };
+
+  renderBody() {
+    return (
+      <div className="body">
+        { this.renderProfile() }
+        { this.renderNextMeetup() }
+      </div>
+    );
+  };
+
+  renderProfile() {
+    return (
+      <div className="row">
+        <img src="https://via.placeholder.com/200x200"/>
+        <div className="row_right">
+          <h3 className="row_right_title">Hacktiv8 Meetup</h3>
+          <div className="row_right_info">
+            <div className="row_right_info_content">
+              <div>Location</div>
+              <div>Members</div>
+              <div>Organizer</div>
+            </div>
+            <div className="row_right_info_content">
+              <div>Jakarta, Indonesia</div>
+              <div>1078</div>
+              <div>Hacktiv8</div>
+            </div>
+          </div>
+          <button className="button">Join Us</button>
+        </div>
+      </div>
+    );
+  };
+
+  renderNextMeetup() {
+    return (
+      <div>
+        <h3>Next Meetup</h3>
+        <div className="column">
+          <h4 style={{marginTop: 0, marginBottom: 10,}}>Awesome meetup and event</h4>
+          <div>25 January 2019</div>
+          <p>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+            <div>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+            <div>It has survived not only five centuries, but also the leap into electronic typesetting.</div>
+            <div>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</div>
+          </p>
+          <div>Lorem Ipsum is simply dummy text of the printing.</div>
+          <p>See you there!</p>
+          <p>Best, Twinky, Winky, Dipsy, Lala, Poh. JakartaJS Organizer.</p>
+        </div>
       </div>
     );
   };
 
   render() {
     return (
-      <div>
+      <div className="root">
         { this.renderHeader() }
-        <h1>Halo, Hacktiv8!</h1>
+        { this.renderBody() }
       </div>
     );
   }
