49a3d027adf96597ef9a992c3255cf6859f67ef7
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-10-09 15:56:03 +0700

    Add snapshot

--- rctn-project/src/pages/Home/index.test.js
+++ rctn-project/src/pages/Home/index.test.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 
 import Home from './';
+import store from "../../configs";
 
 test('Home renders correctly', () => {
   const tree = renderer.create(
-    <Home />,
+    <Provider store={store}>
+      <Home/>
+    </Provider>
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
